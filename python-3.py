c34b2ebb3ec0706d536b91f7aebec04f1edde222
sirichandana2008 <167188605+sirichandana2008@users.noreply.github.com>
2024-04-18 11:28:11 +0530

    Create python-3.py

---
+++ python-3.py
@@ -0,0 +1,12 @@
+python-3.py
+
+class Box:
+    def _init_(self,nameOfStudent,rollNo):
+        self.name = nameOfStudent
+        self.rollno = rollNo
+
+obj1 = Box("varsha",29)
+obj2 = Box("amar",19)
+
+print(obj1.name)
+print(obj2.name)
